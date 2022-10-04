5498682bc96f3cd3679dc91dd04db459ecd7f9b5
AlessianAU <alessia@alessian.org>
2022-10-05 08:56:15 +1100

    Fix model to be in accordance of editor

--- Sources/Notes/Models/Note.swift
+++ Sources/Notes/Models/Note.swift
@@ -4,10 +4,10 @@ final class Note: Identifiable, Codable {
 	var id = UUID()
 	
 	var title: String
-	var text: String
+	var text: [String]
 	var date: String
 	
-	init(id: UUID = UUID(), title: String, text: String, date: String) {
+	init(id: UUID = UUID(), title: String, text: [String], date: String) {
 		self.id = id
 		self.title = title
 		self.text = text
