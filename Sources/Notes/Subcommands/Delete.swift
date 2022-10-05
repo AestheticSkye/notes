60b4d7da15be3b664fe57d54669939c6dff63b4b
AlessianAU <alessia@alessian.org>
2022-10-05 13:45:45 +1100

    Add delete all option

--- Sources/Notes/Subcommands/Delete.swift
+++ Sources/Notes/Subcommands/Delete.swift
@@ -4,13 +4,31 @@ extension Notes {
 	struct Delete: ParsableCommand {
 		static let configuration = CommandConfiguration(abstract: "Delete a note.")
 		
+		@Flag(name: .long)
+		var all: Bool = false
+		
 		@Argument(help: "Name of the note to delete")
-		var name: String
+		var name: String?
+		
+		func validate() throws {
+			if !all && name == nil {
+				throw ValidationError("Please enter a name")
+			}
+		}
 		
 		func run() throws {
 			let persistence = Persistence()
-			
-			persistence.delete(name)
+			if all {
+				print("Are you sure you want to delete all notes? (y/N)")
+				let choice = readLine()
+				if choice?.lowercased() == "y" {
+					for note in persistence.noteData{
+						persistence.delete(note.title)
+					}
+				}
+			} else {
+				persistence.delete(name!)
+			}
 		}
 	}
 }
