5beb38af4de3e49af5bcf61681ff1e1be35bbb46
AlessianAU <alessia@alessian.org>
2022-10-28 10:19:04 +1100

    Add half finished tests

--- Tests/NotesTests/NotesTests.swift
+++ Tests/NotesTests/NotesTests.swift
@@ -2,10 +2,43 @@ import XCTest
 @testable import notes
 
 final class notesTests: XCTestCase {
-    func testPersistence() throws {
-		let persistence = Notes.Persistence()
-//		persistence.noteData?.append(Note(title: "Title", text: [""], date: "date"))
-//		try persistence.save()
-		print(persistence.noteData[0].text)
-    }
+	
+//	private func pressKey(_ keyCode: Int) async {
+//		let keyDown = CGEvent(keyboardEventSource: nil,
+//							  virtualKey: CGKeyCode(keyCode),
+//							  keyDown: true)
+//
+//		let keyUp = CGEvent(keyboardEventSource: nil,
+//							virtualKey: CGKeyCode(keyCode),
+//							keyDown: false)
+//
+//		keyDown?.post(tap: CGEventTapLocation.cghidEventTap)
+//		keyUp?.post(tap: CGEventTapLocation.cghidEventTap)
+//	}
+	
+	func testNew() throws {
+		let process = Process()
+		
+		var executable = FileManager().currentDirectoryPath
+		executable.append(contentsOf: "/.build/debug/notes")
+
+		process.executableURL = URL(fileURLWithPath: executable)
+		process.arguments = ["new"]
+		
+		try process.run()
+		
+		sleep(10)
+		
+		process.terminate()
+		
+		//		Task {
+		//			sleep(1)
+		//			await pressKey(6)
+		//			sleep(1)
+		//			await pressKey(0x35)
+		//		}
+		
+		//		Notes.New().run()
+		//		Notes.Editor()
+	}
 }
